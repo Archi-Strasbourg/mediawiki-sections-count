10a8900d3e8fed3fd960dca07127bd59ad5037f0
Pierre Rudloff <prudloff@insite.coop>
2021-06-01 11:05:03 +0200

    La classe Revision est dépréciée (Insite #26862)

--- SectionsCount.php
+++ SectionsCount.php
@@ -3,8 +3,11 @@
 namespace SectionsCount;
 
 use ContentHandler;
+use MediaWiki\MediaWikiServices;
+use MediaWiki\Revision\RevisionRecord;
+use MediaWiki\Revision\SlotRecord;
+use MWException;
 use Parser;
-use Revision;
 use Title;
 
 class SectionsCount
@@ -13,9 +16,9 @@ class SectionsCount
      * @param Parser $parser
      * @param null $pagename
      * @return int
-     * @throws \MWException
+     * @throws MWException
      */
-    public static function sectionscount(Parser $parser, $pagename = null)
+    public static function sectionscount(Parser $parser, $pagename = null): int
     {
         global $wgTitle, $wgParser, $wgSectionsCountIgnoreSections;
         if (empty($pagename)) {
@@ -25,12 +28,12 @@ class SectionsCount
         }
 
         if (isset($title)) {
-            $revision = Revision::newFromId($title->getLatestRevID());
+            $revision = MediaWikiServices::getInstance()->getRevisionLookup()->getRevisionById($title->getLatestRevID());
             if (isset($revision)) {
                 //Prevent recursive parsing
                 $otherParser = $wgParser->getFreshParser();
                 $nbSections = 0;
-                for ($i = 1; $section = $otherParser->getSection(ContentHandler::getContentText($revision->getContent(Revision::RAW)), $i); $i++) {
+                for ($i = 1; $section = $otherParser->getSection(ContentHandler::getContentText($revision->getContent(SlotRecord::MAIN, RevisionRecord::RAW)), $i); $i++) {
                     if (isset($wgSectionsCountIgnoreSections)) {
                         foreach ($wgSectionsCountIgnoreSections as $ignoreSection) {
                             if (preg_match('/=+\s*' . $ignoreSection . '\s*=+/', $section) == 1) {
@@ -51,7 +54,7 @@ class SectionsCount
 
     /**
      * @param Parser $parser
-     * @throws \MWException
+     * @throws MWException
      */
     public static function onParserSetup(Parser &$parser)
     {
